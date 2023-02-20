c6fa19e9de14ceec3c1bcb2b217879aabd22bb22
Takashi SASAKI <takashi316@gmail.com>
2023-02-20 07:27:37 +0000

    reformatted in Glitch editor.

--- tinyWssModule.js
+++ tinyWssModule.js
@@ -52,66 +52,65 @@ History
 */
 
 function tinyWssModule(wssRelayHost, serviceToken) {
-    if(typeof wssRelayHost !== "string") throw "wssRelayHost is mandatory.";
-    if(typeof serviceToken !== "string") throw "serviceToken is mandatory.";
-    
-    function openWSS(wssUrl) {
-      var socket = new WebSocket(wssUrl);
-      return new Promise(function (okCallback, ngCallback) {
-        socket.addEventListener("open", function (event) {
-          okCallback(socket);
+  if(typeof wssRelayHost !== "string") throw "wssRelayHost is mandatory.";
+  if(typeof serviceToken !== "string") throw "serviceToken is mandatory.";
+  
+  function openWSS(wssUrl) {
+    var socket = new WebSocket(wssUrl);
+    return new Promise(function (okCallback, ngCallback) {
+      socket.addEventListener("open", function (event) {
+        okCallback(socket);
+      });
+    });
+  }
+
+  async function subscribe(channelName) {
+    if(typeof channelName !== "string") throw "channelName is mandatory.";
+
+    var socket = await openWSS(
+      wssRelayHost + "/" + serviceToken + "/" + channelName
+    );
+    console.log(socket);
+    console.log("tinyWssModule:channelOpened");
+    if (wssRelayHost.indexOf("herokuapp.com") > 0) {
+      // Herokuでは55秒ルールでチャンネルが切れるため・・・
+      setTimeout(ping, 45 * 1000);
+    }
+    function onmessage(cbFunc) {
+      socket.addEventListener("message", function (event) {
+        //					console.log('message',event);
+        var json = JSON.parse(event.data);
+        cbFunc({
+          data: json.body,
+          timeStamp: event.timeStamp,
+          origin: event.origin,
+          //						lastEventId: event.lastEventId
         });
       });
     }
-  
-    async function subscribe(channelName) {
-      if(typeof channelName !== "string") throw "channelName is mandatory.";
-  
-      var socket = await openWSS(
-        wssRelayHost + "/" + serviceToken + "/" + channelName
-      );
-      console.log(socket);
-      console.log("tinyWssModule:channelOpened");
-      if (wssRelayHost.indexOf("herokuapp.com") > 0) {
-        // Herokuでは55秒ルールでチャンネルが切れるため・・・
-        setTimeout(ping, 45 * 1000);
-      }
-      function onmessage(cbFunc) {
-        socket.addEventListener("message", function (event) {
-          //					console.log('message',event);
-          var json = JSON.parse(event.data);
-          cbFunc({
-            data: json.body,
-            timeStamp: event.timeStamp,
-            origin: event.origin,
-            //						lastEventId: event.lastEventId
-          });
-        });
-      }
-      function send(msg) {
-        //				console.log("sendMsg:",msg,"  typeof(msg)",typeof(msg));
-        var outMsg = { body: msg };
-        outMsg = JSON.stringify(outMsg);
-        //				console.log("sendMsg:",outMsg);
-        socket.send(outMsg);
-      }
-      function ping() {
-        // Herokuでのコネクション維持用ヌルメッセージ
-        socket.send("");
-        setTimeout(ping, 45 * 1000);
-      }
-      return {
-        serverName: wssRelayHost,
-        set onmessage(cbf) {
-          onmessage(cbf);
-        },
-        send: send,
-      };
-    } //async function subscribe
+    function send(msg) {
+      //				console.log("sendMsg:",msg,"  typeof(msg)",typeof(msg));
+      var outMsg = { body: msg };
+      outMsg = JSON.stringify(outMsg);
+      //				console.log("sendMsg:",outMsg);
+      socket.send(outMsg);
+    }
+    function ping() {
+      // Herokuでのコネクション維持用ヌルメッセージ
+      socket.send("");
+      setTimeout(ping, 45 * 1000);
+    }
     return {
-      subscribe: subscribe,
+      serverName: wssRelayHost,
+      set onmessage(cbf) {
+        onmessage(cbf);
+      },
+      send: send,
     };
-  } //function tinyWssModule
-  
-  export { tinyWssModule };
-  
+  } //async function subscribe
+  return {
+    subscribe: subscribe,
+  };
+} //function tinyWssModule
+
+export { tinyWssModule };
