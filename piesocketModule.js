c12aaf6694328565c661a8c36619d60d99ecbea9
Takashi SASAKI <takashi316@gmail.com>
2023-02-20 07:15:13 +0000

    reformatted in Glitch editor

--- piesocketModule.js
+++ piesocketModule.js
@@ -52,65 +52,64 @@ History
 */
 
 function piesocketModule(ClusterID, serviceToken) {
-    if (typeof serviceToken !== "string") throw "serviceToken is mandatory.";
-  
-    var socket;
-    if (!ClusterID) {
-      ClusterID = "demo";
-    }
-    function open(channelName) {
-      //			var channelNumber = crc16(channelName);
-      var channelNumber = channelName;
-      socket = new WebSocket(
-        "wss://" +
-          ClusterID +
-          ".piesocket.com/v3/" +
-          channelNumber +
-          "?apiKey=" +
-          serviceToken
-      );
-      return new Promise(function (okCallback, ngCallback) {
-        socket.addEventListener("open", function (event) {
-          okCallback(true);
+  if (typeof serviceToken !== "string") throw "serviceToken is mandatory.";
+
+  var socket;
+  if (!ClusterID) {
+    ClusterID = "demo";
+  }
+  function open(channelName) {
+    //			var channelNumber = crc16(channelName);
+    var channelNumber = channelName;
+    socket = new WebSocket(
+      "wss://" +
+        ClusterID +
+        ".piesocket.com/v3/" +
+        channelNumber +
+        "?apiKey=" +
+        serviceToken
+    );
+    return new Promise(function (okCallback, ngCallback) {
+      socket.addEventListener("open", function (event) {
+        okCallback(true);
+      });
+    });
+  }
+  async function subscribe(channelName) {
+    if (typeof channelName !== "string") throw "channelName is mandatory.";
+
+    await open(channelName);
+    console.log("piesocketModule:channelOpened");
+    function onmessage(cbFunc) {
+      socket.addEventListener("message", function (event) {
+        //					console.log('message',event);
+        var json = JSON.parse(event.data);
+        cbFunc({
+          data: json.body,
+          timeStamp: event.timeStamp,
+          origin: event.origin,
+          //						lastEventId: event.lastEventId
         });
       });
     }
-    async function subscribe(channelName) {
-      if (typeof channelName !== "string") throw "channelName is mandatory.";
-  
-      await open(channelName);
-      console.log("piesocketModule:channelOpened");
-      function onmessage(cbFunc) {
-        socket.addEventListener("message", function (event) {
-          //					console.log('message',event);
-          var json = JSON.parse(event.data);
-          cbFunc({
-            data: json.body,
-            timeStamp: event.timeStamp,
-            origin: event.origin,
-            //						lastEventId: event.lastEventId
-          });
-        });
-      }
-      function send(msg) {
-        //				console.log("sendMsg:",msg,"  typeof(msg)",typeof(msg));
-        var outMsg = { body: msg };
-        outMsg = JSON.stringify(outMsg);
-        //				console.log("sendMsg:",outMsg);
-        socket.send(outMsg);
-      }
-      return {
-        serverName: "websocket.in",
-        set onmessage(cbf) {
-          onmessage(cbf);
-        },
-        send: send,
-      };
+    function send(msg) {
+      //				console.log("sendMsg:",msg,"  typeof(msg)",typeof(msg));
+      var outMsg = { body: msg };
+      outMsg = JSON.stringify(outMsg);
+      //				console.log("sendMsg:",outMsg);
+      socket.send(outMsg);
     }
     return {
-      subscribe: subscribe,
+      serverName: "websocket.in",
+      set onmessage(cbf) {
+        onmessage(cbf);
+      },
+      send: send,
     };
   }
-  
-  export { piesocketModule };
-  
+  return {
+    subscribe: subscribe,
+  };
+}
+
+export { piesocketModule };
