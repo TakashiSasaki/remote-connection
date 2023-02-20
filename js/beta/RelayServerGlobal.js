1d9f8b7554361f3b92edd0d3adccce238cab2639
Takashi SASAKI <takashi316@gmail.com>
2023-02-20 12:14:44 +0900

    コメント追加。

--- js/beta/RelayServerGlobal.js
+++ js/beta/RelayServerGlobal.js
@@ -1,2 +1,4 @@
+// This module comes from https://github.com/TakashiSasaki/remote-connection .
+// Originally written by https://github.com/satakagi .
 import {RelayServer} from "./RelayServer.js";
 window.RelayServer = RelayServer;
